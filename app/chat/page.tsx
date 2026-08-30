54661c83594cb68d01ec73d5f422455101a519a7
adarshkasaundhan1 <adarshkasaundhan1@gmail.com>
2026-08-31 00:07:23 +0530

    Update page.tsx

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 import { useEffect, useRef, useState } from "react";
 import { useSearchParams } from "next/navigation";
 import { io, Socket } from "socket.io-client";
